2ec934132d5d65649eaff08d0906a5feb590079c
MathiasClari <mathiasclari@noximity.com>
2024-01-14 16:17:48 +0100

    Remove game mode commands and add Qodana workflow file
    This commit removes several game mode command files such as GMCommands, GamemodeSpectator, and others. It also adds a new file for Qodana code quality checks under a GitHub workflows directory. Additionally, other files like CommandManager.java and plugin.yml have been modified consistent with the removal of game mode commands. Furthermore, a new CherryCoreProxy.java file has been introduced.

---
+++ src/main/java/net/cherrycraft/cherrycoreproxy/command/CommandRegister.java
@@ -0,0 +1,23 @@
+/*
+ * (c) 2024 CherryCraft. All rights reserved.
+ *
+ *  This software is the confidential and proprietary information of CherryCraft
+ *  ("Confidential Information"). You shall not disclose such Confidential Information
+ *  and shall use it only in accordance with the terms of the license agreement you
+ *  entered into with CherryCraft.
+ *
+ *  UNAUTHORIZED COPYING, DISTRIBUTION, OR REPRODUCTION OF THIS SOFTWARE, IN WHOLE OR
+ *  IN PART, IS STRICTLY PROHIBITED. UNLESS OTHERWISE EXPRESSLY AGREED UPON IN A
+ *  WRITTEN AGREEMENT, CHERRYCRAFT PROVIDES THIS SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY
+ *  KIND, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ *  NON-INFRINGEMENT.
+ *
+ *  For inquiries, please contact CherryCraft at copyright@cherrycraft.net.
+ */
+
+package net.cherrycraft.cherrycoreproxy.command;
+
+public class CommandRegister {
+
+}
